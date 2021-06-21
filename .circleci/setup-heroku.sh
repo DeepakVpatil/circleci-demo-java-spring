0c491d8c39d11f2fad362a0d4ffe9448a5f52b56
DeepakVpatil <44339930+DeepakVpatil@users.noreply.github.com>
2021-06-21 15:06:06 +0530

    Create setup-heroku.sh

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget -qO- https://cli-assets.heroku.com/install.sh | sh
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
+
+heroku git:remote -a $HEROKU_APP
